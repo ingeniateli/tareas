4013ae61f073f8883b913f73b82e18b1619f4a75
ingeniateli <ingeniateli@gmail.com>
2025-09-14 18:28:02 -0300

    cambio texto

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hola Mundo desde Express en Vercel!");
+    res.send("Hola en desarrollo Vercel!");
 });
 
 export default app;
